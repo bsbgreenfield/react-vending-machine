08e0d5a212c556711f6cc42f97eac28c8212b3b2
Benjamin Greenfield <benjamingreenfield@Benjamins-MacBook-Pro.local>
2023-05-10 15:53:31 -0700

    add features

---
+++ src/Apple.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Items.css"
+function Apple(){
+    return(
+        <div className="Item">
+            Apple
+        </div>
+    )
+}
+
+export default Apple
---
+++ src/Carrot.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Items.css"
+function Carrot(){
+    return(
+        <div className="Item">
+            Carrot
+        </div>
+    )
+}
+
+export default Carrot
